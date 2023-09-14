7e5ea61ce5689307a6c6cc412c21be0f2d7fd618
rbamarc <rbamarc929@gmail.com>
2023-09-14 05:38:50 -0700

    working on search

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,37 +1,41 @@
 import { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
 
-function ProductList() {
-    const [products, setProduct] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+function ProductList({ filter = "" }) {  // Added filter prop with a default value of an empty string
+    const [products, setProduct] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(`https://fakestoreapi.com/products`)
+                const response = await fetch(`https://fakestoreapi.com/products`);
                 
-                const data = await response.json()
-                setProduct(data)
+                const data = await response.json();
+                setProduct(data);
             } catch (error) {
-                console.error('there was a problem with the fetch operation:', error)
+                console.error('there was a problem with the fetch operation:', error);
             }
-            setIsLoading(false)
+            setIsLoading(false);
         }
-        fetchData()
-    }, [])
+        fetchData();
+    }, []);
     
+    const filteredProducts = products.filter(product => 
+        product.title.toLowerCase().includes(filter.toLowerCase())
+    );
+
     return (
         <div className="main-content">
             <h1>Product List</h1>
             {isLoading ? (
                 <p>Loading...</p>
             ) : (     
-                products.map((product) => (
-                    <ProductCard key={product.id} product={ product} />
+                filteredProducts.map((product) => (  // Render the filtered products
+                    <ProductCard key={product.id} product={product} />
                 ))      
             )}
         </div>
-    )
+    );
 }
 
-export default ProductList
+export default ProductList;
