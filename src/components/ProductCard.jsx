98e926081e33cd9f7f5e1f6b82a16f4e819f4862
rbamarc <rbamarc929@gmail.com>
2023-09-18 05:54:21 -0700

    more pretty

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,11 +3,24 @@ import { Link } from 'react-router-dom';
 
 function ProductCard({ product }) {
   return (
-    <Card className='product-card' style={{ width: '12rem', margin: '2rem', padding: '1rem' }}>
+    <Card 
+        className='product-card' 
+        style={{ 
+            width: '12rem', 
+            margin: '2rem', 
+            padding: '1rem', 
+            boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' // This is the shadow
+        }}
+    >
       <Card.Img 
         variant="top" 
         src={product.image} 
-        style={{ width: '150px', height: '150px', objectFit: 'cover', transition: 'transform 0.3s ease-in-out' }} 
+        style={{ 
+            width: '150px', 
+            height: '150px', 
+            objectFit: 'cover', 
+            transition: 'transform 0.3s ease-in-out' 
+        }} 
         className="product-image"
       />
       <Card.Body>
